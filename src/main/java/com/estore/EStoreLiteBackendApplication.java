0ec2d1e58f1501325899ed0952e75f60c5478ecf
Safwan Rashid <safwanrashid.t@gmail.com>
2022-04-22 17:28:01 +0530

    initial commit

---
+++ src/main/java/com/estore/EStoreLiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreLiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStoreLiteBackendApplication.class, args);
+	}
+
+}
